87d41b319f6f6a01d1fcd77d5d32199260a0e284
Salama <malach.salama@gmail.com>
2024-06-20 14:07:34 +0300

    Implemented webSocket to update notifications and activate lpo receive button synchronously

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const routes = require("./routes/index");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const http = require("http"); // Import http to create a server
+const socketIo = require("socket.io"); // Import Socket.IO
 
 require("dotenv").config();
 const port = process.env.PORT || 8000;
@@ -16,21 +18,40 @@ const corsOptions = {
 // App init
 const app = express();
 
+// Create an HTTP server
+const server = http.createServer(app);
+
+// Initialize Socket.IO with the HTTP server
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
 // Middlewares
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
 
+// Pass io to routes
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 // Routes
 app.use("/api", routes);
 
+// MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`Server listening on port ${port}`);
     });
   })
